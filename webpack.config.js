e38ebf91b763ba17d7859850141e6d71ef9d20da
tjonesutd87 <tjonesutd87@gmail.com>
2024-11-09 08:56:05 -0600

    add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    devtool: 'eval-souce-map',
+    devServer: {
+        watchFiles: ['./src/template.html'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                user: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+};
