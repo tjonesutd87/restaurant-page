455e250ebdeafe97540190dddaa57b08ebabfe88
tjonesutd87 <tjonesutd87@gmail.com>
2024-11-09 08:24:35 -0600

    add src folder and index.js entry point

---
+++ src/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+        }),
+    ],
+};
