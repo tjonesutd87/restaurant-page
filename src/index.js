deeaf4409c5fbc4ec8c804d300db97c7d272f902
tjonesutd87 <tjonesutd87@gmail.com>
2024-11-15 14:38:10 -0600

    add other remaining modules to index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './styles.css';
+import { initialPageLoad } from './initial-load.js';
+import { aboutPageLoad } from './about.js';
+import { menuPageLoad } from './menu.js';
+import { contactPageLoad } from './contact.js';
+
+initialPageLoad();
