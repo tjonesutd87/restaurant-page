b115b7afde6cf236fbdfee8918b11977ae3a8555
tjonesutd87 <tjonesutd87@gmail.com>
2024-11-15 14:25:37 -0600

    fixed logic issues in image and attribution formatter modules

--- src/attribution-formatter.js
+++ src/attribution-formatter.js
@@ -1,13 +1,22 @@
-export function attributionFormatter(parentId, outerElement, description, artist = '', artistUrl = '', source, sourceUrl) {
+//  this function takes various arguments to create a formatted attribute and append it to a parent element.
+export function attributionFormatter(parentId, outerElement, description, source, sourceUrl, artist = '', artistUrl = '') {
+    //  declare variables, creating/getting dom elements
     const parent = document.getElementById(parentId);
     const attribution = document.createElement(outerElement);
     const descriptionText = description + ' ';
     const attributionDesc = document.createTextNode(descriptionText);
     const onText = document.createTextNode(' on ');
     const sourceLink  = document.createElement('a');
+
+    //  set source link hyper reference to source url and set the text
+    //  content to source name then append the attribution to the parent dom element
     sourceLink.href = sourceUrl;
     sourceLink.textContent = source;
     parent.appendChild(attribution);
+
+    //  if the artist is not passed in arguments, then just format the attribution with the description followed by the source link
+    //  otherwise format attribution with description followed by artist link followed by the text ' on ' then the source link.
+    //  The attribution elements are appeneded to the attribution depending on how it is formatted.
     if (artist == ''){
         attribution.appendChild(attributionDesc);
         attribution.appendChild(sourceLink);
--- src/image-formatter.js
+++ src/image-formatter.js
@@ -1,20 +1,36 @@
 import { attributionFormatter } from './attribution-formatter.js';
 
-export function imageFormatter (parentId, source, width, height, description, artist = '', artistUrl = '', sourceName, sourceUrl, imageId, imageClass = '') {
+export function imageFormatter (parentId, source, width, height, imageId,  caption, description = '', artist = '', artistUrl = '', sourceName = '', sourceUrl = '', imageClass = '') {
+    //  declare variables
     const parent = document.getElementById(parentId);
     const image = document.createElement('img');
     const imageWidth = width +'px';
     const imageHeight = height + 'px';
+
+    //  if a caption was included, then create a figure and append the image to the figure
+    //  run the attribution formatter to format the attribution and then append to figure as a figcaption
+    //  if no caption the just append the image to the parent element.
+    if (caption) {
     const figure = document.createElement('figure');
+        //  set image source, height/width, and figure id
+        figure.id = imageId;
+
+        //  append figure to content and append image to figure, then run the attribution formatter which will append the attribution to the figure as a figcaption
+        parent.appendChild(figure);
+        figure.appendChild(image);
+        attributionFormatter(imageId, 'figcaption', description, sourceName, sourceUrl, artist, artistUrl);
+    } else {
+        parent.appendChild(image);
+        image.id = imageId;
+    }
+
+    //  set image source and size
     image.src = source;
     image.style.width = imageWidth;
     image.style.height = imageHeight;
-    figure.id = imageId;
+
+    //  set image classes if there are any
     if (imageClass !== '') {
         image.classList = imageClass;
     }
-    parent.appendChild(figure);
-    figure.appendChild(image);
-    attributionFormatter(imageId, 'figcaption', description, artist, artistUrl, sourceName, sourceUrl);
-    return attributionFormatter;
 }
