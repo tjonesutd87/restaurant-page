54c1b88253a1ecadc6e1e64e206f28c883f8ee5d
tjonesutd87 <tjonesutd87@gmail.com>
2024-11-19 12:27:59 -0600

    create menu object to house menu options

---
+++ src/menu.js
@@ -0,0 +1,139 @@
+import caprese from './images/caprese.jpg';
+import antipasto from './images/antipasto.jpg';
+import gnocchi from './images/gnocchi.jpg';
+import lasagna from './images/lasagna.jpg';
+import spaghetti from './images/spaghetti.jpg';
+import farfalle from './images/farfalle.jpg';
+import cheesePizza from './images/cheese-pizza.jpg';
+import pepperoni from './images/pepperoni.jpg';
+import chefSpecialPizza from './images/chef-special-pizza.jpg';
+
+
+export const menu = [
+    {
+        sectionName: 'appetizers',
+        item: [
+            {
+                itemName: 'Caprese',
+                itemDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                img: {
+                    imgSource: caprese,
+                    imgDesc: 'Photo By ',
+                    artist: 'Markus Spiske',
+                    artistUrl: 'https://unsplash.com/@markusspiske?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+                    source: 'Unsplash',
+                    sourceUrl: 'https://unsplash.com/photos/closeup-photo-of-tofu-and-tomato-slices-_GM0Zvw3PzY?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+                },
+            },
+            {
+                itemName: 'Antipasto',
+                itemDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                img: {
+                    imgSource: antipasto,
+                    imgDesc: 'Photo By ',
+                    artist: 'David B Townsend',
+                    artistUrl: 'https://unsplash.com/@dbtownsend?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+                    source: 'Unsplash',
+                    sourceUrl: 'https://unsplash.com/photos/sliced-meat-on-brown-wooden-chopping-board-ppN7TyeaQK0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+                },
+            },
+        ]
+
+    },
+    {
+        sectionName: 'pizza',
+        item: [
+            {
+                itemName: 'Cheese',
+                itemDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                img: {
+                    imgSource: cheesePizza,
+                    imgDesc: 'Photo By ',
+                    artist: 'Ivan Torres',
+                    artistUrl: 'https://unsplash.com/@iavnt?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+                    source: 'Unsplash',
+                    sourceUrl: 'https://unsplash.com/photos/pizza-with-berries-MQUqbmszGGM?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+                },
+            },
+            {
+                itemName: 'Pepperoni',
+                itemDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                img: {
+                    imgSource: pepperoni,
+                    imgDesc: 'Photo By ',
+                    artist: 'Alan Hardman',
+                    artistUrl: 'https://unsplash.com/@alanaktion?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+                    source: 'Unsplash',
+                    sourceUrl: 'https://unsplash.com/photos/sliced-meat-on-brown-wooden-chopping-board-ppN7TyeaQK0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+                },
+            },
+            {
+                itemName: 'Chef\'s Special',
+                itemDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                img: {
+                    imgSource: chefSpecialPizza,
+                    imgDesc: 'Photo By ',
+                    artist: 'amirali mirhashemia',
+                    artistUrl: 'https://unsplash.com/@amir_v_ali?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+                    source: 'Unsplash',
+                    sourceUrl: 'https://unsplash.com/photos/sliced-meat-on-brown-wooden-chopping-board-ppN7TyeaQK0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+                },
+            },
+        ]
+    },
+    {
+        sectionName: 'pasta',
+        item: [
+            {
+                itemName: 'Gnocchi',
+                itemDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                img: {
+                    imgSource: gnocchi,
+                    imgDesc: 'Photo By ',
+                    artist: 'Markus Spiske',
+                    artistUrl: 'https://unsplash.com/@markusspiske?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+                    source: 'Unsplash',
+                    sourceUrl: 'https://unsplash.com/photos/closeup-photo-of-tofu-and-tomato-slices-_GM0Zvw3PzY?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+                },
+            },
+            {
+                itemName: 'Lasagna',
+                itemDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                img: {
+                    imgSource: lasagna,
+                    imgDesc: 'Photo By ',
+                    artist: 'David B Townsend',
+                    artistUrl: 'https://unsplash.com/@dbtownsend?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+                    source: 'Unsplash',
+                    sourceUrl: 'https://unsplash.com/photos/sliced-meat-on-brown-wooden-chopping-board-ppN7TyeaQK0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+                },
+            },
+            {
+                itemName: 'Spaghetti',
+                itemDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                img: {
+                    imgSource: spaghetti,
+                    imgDesc: 'Photo By ',
+                    artist: 'David B Townsend',
+                    artistUrl: 'https://unsplash.com/@dbtownsend?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+                    source: 'Unsplash',
+                    sourceUrl: 'https://unsplash.com/photos/sliced-meat-on-brown-wooden-chopping-board-ppN7TyeaQK0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+                },
+            },
+            {
+                itemName: 'Farfalle',
+                itemDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                img: {
+                    imgSource: farfalle,
+                    imgDesc: 'Photo By ',
+                    artist: 'David B Townsend',
+                    artistUrl: 'https://unsplash.com/@dbtownsend?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+                    source: 'Unsplash',
+                    sourceUrl: 'https://unsplash.com/photos/sliced-meat-on-brown-wooden-chopping-board-ppN7TyeaQK0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+                },
+            },
+        ]
+
+    },
+];
+
