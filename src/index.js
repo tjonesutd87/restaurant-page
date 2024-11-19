cd16a3f85f56ba9908520647fbe2ef4150ec13a5
tjonesutd87 <tjonesutd87@gmail.com>
2024-11-19 12:25:49 -0600

    update nav button functionality to create and remove container element

--- src/index.js
+++ src/index.js
@@ -1,7 +1,37 @@
 import './styles.css';
 import { initialPageLoad } from './initial-load.js';
 import { aboutPageLoad } from './about.js';
-import { menuPageLoad } from './menu.js';
+import { menuPageLoad } from './menuPage.js';
 import { contactPageLoad } from './contact.js';
 
+
+//  declare variables
+const aboutBtn = document.getElementById('about');
+const menuBtn = document.getElementById('menu');
+const contactBtn = document.getElementById('contact');
+const homeBtn = document.getElementById('home');
+
+//  run intial page load module to load initial homepage
 initialPageLoad();
+
+aboutBtn.addEventListener('click', () => {
+    document.body.removeChild(container);
+    aboutPageLoad();
+});
+
+homeBtn.addEventListener('click', () => {
+    document.body.removeChild(container);
+    initialPageLoad();
+});
+
+menuBtn.addEventListener('click', () => {
+    document.body.removeChild(container);
+    menuPageLoad();
+});
+
+
+contactBtn.addEventListener('click', () => {
+    document.body.removeChild(container);
+    contactPageLoad();
+});
+
