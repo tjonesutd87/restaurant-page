55dcc2eed8cfecded62738bf6bd0a4cbe0f0cc2c
tjonesutd87 <tjonesutd87@gmail.com>
2024-11-25 14:36:49 -0600

    create contact page builder module

--- src/contact.js
+++ src/contact.js
@@ -15,6 +15,11 @@ export function contactPageLoad() {
    const titleText = document.createElement('h1');
    const contactDiv = document.createElement('div');
    const contactTitle = document.createElement('h2');
+   const contactTitleDiv = document.createElement('div');
+   const formInfo = document.createElement('div');
+   const info = [{infoType: 'Phone', infoValue: '867-5309',}, {infoType: 'Email', infoValue: 'test@test.test',}];
+   const infoVerbiage = 'You can use the below information to contact us, or you can submit a message below.';
+   const infoP = document.createElement('p');
 
    //  append site title
    container.appendChild(titleDiv);
@@ -24,11 +29,15 @@ export function contactPageLoad() {
 
    //  append maint content div
    container.appendChild(content);
-   content.id = 'menu-content';
+   content.id = 'contact-content';
 
     //  append contact section and title
     content.appendChild(contactDiv);
-    contactDiv.appendChild(contactTitle);
+    contactDiv.appendChild(contactTitleDiv);
+    contactTitleDiv.id = 'contact-title';
+    contactTitleDiv.appendChild(contactTitle);
     contactDiv.id = 'contact-div';
     contactTitle.textContent = 'Contact';
+
+
 }
